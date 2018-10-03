0cbbe792456c223990b24da1c112aaff4b116d61
Christian Whitted <TheVoidLives@users.noreply.github.com>
2018-10-03 15:47:26 -0400

    improved event query for deployment

--- backend/routes/events.js
+++ backend/routes/events.js
@@ -4,144 +4,109 @@ var mongoose = require('mongoose');
 var User = require('mongoose').model('User');
 var Event = require('mongoose').model('Event');
 
-exports.plugin =
-    {
-        name: 'routes-events',
-        register: async function(server, options, next) {
 
-            // Routes
-            // ------------------
-            // GET
-            server.route({
-                method: 'GET',
-                path: '/events',
-                handler: async (request,h) =>
-                {
-                    // TODO: Validate Query
-                    var query = request.query;
+async function validate (payload) {
+    // TODO: Validate an update payload
+    // Likely using JOI
+    return false;
+}
 
-                    // Query based on distance
-                    try {
-                        var events;
-                        if (query.lon && query.lat && query.dist) {
-                           events = Event.findNearby([query.lon, query.lat], query.dist * 1609.34);
-                        }
-                        else {
-                           events = Event.find().limit(15).exec();
-                        }
+exports.plugin = {
+
+    name: 'routes-events',
+    version: '1.0.1',
+    register: async function(server, options, next) {
+
+        // Routes
+        // ------------------
+        // GET
+        server.route({
+            method: 'GET',
+            path: '/events',
+            handler: async (request,h) =>
+            {
+                // TODO: Validate Query
+                var query = request.query;
+
+                // Query based on distance
+                try {
+                    if (query.lon && query.lat && query.dist) {
+                        return Event.findNearby([query.lon, query.lat], query.dist * 1609.34);
                     }
-                    catch (err) {
-                        console.log(err);
-                        return h.response('Malformed request could not be processed').code(400);
+                    else {
+                        return Event.find().limit(15).exec();
                     }
-                    return h.response(events).code(200);
                 }
-            });
-
-
-            server.route({
-                method: 'GET',
-                path: '/events/{id}',
-                handler: async function (request,h)
-                {
-                    // TODO: Validate Query
-                    return Event.find({ _id: request.query.id}).exec();
+                catch (err) {
+                    console.log(err);
+                    return h.response('Malformed request could not be processed').code(400);
                 }
-            });
+
+            }
+        });
 
 
-            // Routes
-            // ------------------
-            // PUT
-            server.route({
-                method: 'PUT',
-                path: '/events',
-                handler: async function (request,h)
-                {
-                    var payload = request.payload;
+        server.route({
+            method: 'GET',
+            path: '/events/{id}',
+            handler: async function (request,h)
+            {
+                // TODO: Validate Query
+                return Event.find({ _id: request.query.id}).exec();
+            }
+        });
 
-                    // if !validate(payload) return h.response('Nice Try fam..').code(400);
 
-                    var event = new Event({
-                        name: payload.name,
-                        desc: payload.desc,
-                        time: {
-                            start: payload.start, 
-                            end: payload.end
-                        },
-                        loc: payload.loc,
-                        host: payload.host,
-                        members: payload.members,
-                        capacity: payload.capacity
+        // Routes
+        // ------------------
+        // POST
+        server.route({
+            method: 'POST',
+            path: '/events/{id}',
+            handler: async function (request,h)
+            {
+                // TODO: Validate
+                var payload = request.payload;
+
+                // if !validate(payload) return h.response('Nice Try fam..').code(400);
+
+                var event = Event.findByIdAndUpdate(
+                    encodeURIComponent(request.params.id), 
+                    payload, 
+                    (err) => {
+                        if (err) return h.response(err).code(400);
                     });
 
-                    console.log(event);
-
-                    try {
-                        var response =  event.save(err => { 
-                            if (err) return err;
-                        });
-                        return h.
-                            response(`Successfully Created ${encodeURIComponent(request.params.name)}`)
-                            .code(201);
-                    } 
-                    catch(error) { 
-                        console.error(error);
-                        return h.response('Request could not be processed').code(400);
-                    }
-                }
-            });
-
-            // Routes
-            // ------------------
-            // POST
-            server.route({
-                method: 'POST',
-                path: '/events/{id}',
-                handler: async function (request,h)
-                {
-                    // TODO: Validate
-                    var payload = request.payload;
-
-                    // if !validate(payload) return h.response('Nice Try fam..').code(400);
-
-                    var event = Event.findByIdAndUpdate(
-                        encodeURIComponent(request.params.id), 
-                        payload, 
-                        (err) => {
-                            if (err) return h.response(err).code(400);
-                        });
-
-                    return h
-                        .response(`Successfully Updated event: ${encodeURIComponent(event.name)}`)
-                        .code(201);
-                }
-            });
+                return h
+                    .response(`Successfully Updated event: ${encodeURIComponent(event.name)}`)
+                    .code(201);
+            }
+        });
 
 
-            // Routes
-            // ------------------
-            // DELETE
-            server.route({
-                method: 'DELETE',
-                path: '/events/{id}',
-                handler: async function (request,h)
-                {
-                    var payload = request.payload;
+        // Routes
+        // ------------------
+        // DELETE
+        server.route({
+            method: 'DELETE',
+            path: '/events/{id}',
+            handler: async function (request,h)
+            {
+                var payload = request.payload;
 
-                    // if !validate(payload) return h.response('Nice Try fam..').code(400);
+                // if !validate(payload) return h.response('Nice Try fam..').code(400);
 
-                    var event = Event.findByIdAndDelete(
-                        encodeURIComponent(request.params.id), 
-                        payload, 
-                        (err) => {
-                            if (err) return h.response(err).code(400);
-                        });
+                var event = Event.findByIdAndDelete(
+                    encodeURIComponent(request.params.id), 
+                    payload, 
+                    (err) => {
+                        if (err) return h.response(err).code(400);
+                    });
 
-                    return h
-                        .response(`Successfully removed event: ${encodeURIComponent(event.name)}`)
-                        .code(203);
-                }   
-            });
-        }
-    };
+                return h
+                    .response(`Successfully removed event: ${encodeURIComponent(event.name)}`)
+                    .code(203);
+            }   
+        });
+    }
+};
--- backend/server.js
+++ backend/server.js
@@ -4,39 +4,25 @@ const Hapi = require('hapi');
 const db  = require('./db').db;
 const Event = require('mongoose').Event;
 const User = require('./models/user.js').User;
-const Even = require('mongoose').model('Event');
 
 
 const server = Hapi.server({
-   port: 3000,
-   host: 'localhost'
+    port: 3000,
+    host: 'localhost'
 });
 
 
 const init = async () => {
 
-   // Register route "plugins"
-   await server.register([  
-      require('./routes/users'),
-      require("./routes/events"),
-   ]);
+    // Register route "plugins"
+    await server.register([
+        require('./routes/defaults'),
+        require('./routes/users'),
+        require("./routes/events"),
+    ]);
 
-   Even.create({
-      name: 'testing',
-      desc: 'tested desc 2', 
-      time: {
-         end: Date.now
-      },
-   },
-      function(err, event) {
-         if (err) 
-            return err;
-
-         console.log(event);
-      });
-
-   await server.start();
-   console.log(`Server running at: ${server.info.uri}`);
+    await server.start();
+    console.log(`Server running at: ${server.info.uri}`);
 };
 
 init();
