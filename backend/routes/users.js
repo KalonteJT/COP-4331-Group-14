cbf94d9b32bfdfe817d87962b95464fc453f1a5d
Christian Whitted <TheVoidLives@users.noreply.github.com>
2018-11-10 18:57:17 -0500

    final backend fix

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -17,8 +17,21 @@ exports.plugin = {
             path: '/users',
             handler: async function (request,h)
             {
-                // TODO: Validate Query
-                return User.find().limit(15).exec();
+
+                const query = request.query;
+                console.log(query);
+
+                if (query.id) {
+                    return User
+                        .findOne({ '_id': encodeURIComponent(query.id)}).exec();
+                } else if (query.email) {
+                    return User
+                        .findOne({ email: query.email}).exec();
+                } else {
+                    return h
+                        .response('insufficient params')
+                        .code(400)
+                }
             }
         });
 
@@ -33,7 +46,6 @@ exports.plugin = {
             {
                 // TODO: Validate
                 var payload = request.payload;
-
 
                 try {
                     // if !validate(payload) return h.response('Nice Try fam..').code(400);
@@ -56,29 +68,35 @@ exports.plugin = {
         // POST
         server.route({
             method: 'POST',
-            path: '/users/{id}',
+            path: '/users',
             handler: async function (request,h)
             {
                 // TODO: Validate
+                const query = request.query;
                 var payload = request.payload;
 
                 // if !validate(payload) return h.response('Nice Try fam..').code(400);
-                if (request.params.id) {
+                if (query.id) {
+                    return User.findByIdAndUpdate(
+                        encodeURIComponent(query.id), 
+                        payload,
+                        (err) => {
+                            if (err) return h.response(err).code(400);
+                        });
+                } else if (query.email) {
+
+                    return User.findOneAndUpdate(
+                        { email: query.email}, 
+                        payload,
+                        (err) => {
+                            if (err) return h.response(err).code(400);
+                        });
+                }
+                else {
                     return h
                         .response(`No ID submitted.`)
                         .code(401);
                 }
-
-                var user = User.findByIdAndUpdate(
-                    encodeURIComponent(request.params.id), 
-                    payload,
-                    (err) => {
-                        if (err) return h.response(err).code(400);
-                    });
-
-                return h
-                    .response(user)
-                    .code(201);
             }
         });
 
@@ -90,12 +108,24 @@ exports.plugin = {
             path: '/users',
             handler: async function (request,h)
             {
-                var params = request.params;
+                var query = request.query;
+                if (query.id) {
+                    // TODO: Remove a user from the DB. Err if user DNE.
+                    return User.deleteOne({ '_id': query.id}, function (err) {
+                        if (err) return h.response(err).code(400);
+                    });
+                } else if (query.email) {
+                    // TODO: Remove a user from the DB. Err if user DNE.
+                    return User.deleteOne({ 'email': query.email}, function (err) {
+                        if (err) return h.response(err).code(400);
+                    });
+                }
+                else {
+                    return h
+                        .response(`No ID submitted.`)
+                        .code(401);
+                }
 
-                // TODO: Remove a user from the DB. Err if user DNE.
-                return User.deleteOne({ _id: params.id}, function (err) {
-                    if (err) return h.response(err).code(400);
-                })
             }
         });
     }
