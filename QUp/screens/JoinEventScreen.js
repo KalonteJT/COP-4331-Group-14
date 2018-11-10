ab970da6c93d6dc3e0c6f4f04247eb1e99a82959
crussell122 <42820804+crussell122@users.noreply.github.com>
2018-11-09 22:44:29 -0500

    added join functionality (not finished)

--- QUp/screens/JoinEventScreen.js
+++ QUp/screens/JoinEventScreen.js
@@ -9,12 +9,20 @@ import {saveUserId, getUserId} from '../utils/Storage';
 
 export default class JoinEventScreen extends React.Component {
 
+    constructor(props){
+      super(props);
+      this.state={
+        eventMembers: [],
+      }
+    }
+
+
     static navigationOptions = ({navigation}) => {
         return {
           headerTitle: "Event Details",
           headerRight: (
             <View style={styles.buttonStyle}>
-              <Button onPress={() => navigation.navigate('NewEvent')}
+              <Button onPress={() => {navigation.state.params.join()}}
               title="Join Event!"
               color="#53575e"
               />
@@ -23,10 +31,46 @@ export default class JoinEventScreen extends React.Component {
         };
       };
 
+    componentDidMount() {
+       this.props.navigation.setParams({join: this.joinEvent.bind(this)})
+    }
+
+    joinEvent() {
+      const {params} = this.props.navigation.state;
+      var apiKey = "6q0GvT04E_mFKH1XqLKO31Sw_6bw0i_Y";
+      var myDB = "qupdb";
+      var myCollection = "Events";
+      var eventId = params.item._id.$oid;
+      var url = "https://api.mlab.com/api/1/databases/"+myDB+"/collections/"+myCollection+"?q={_id:{$oid:'"+eventId+"'}}&apiKey="+apiKey
+      console.log(url);
+
+      fetch(url)
+      .then((res) => res.json())
+      .then((res) => {
+        getUserId().then((result) => 
+        fetch(url ,{
+          method: 'PUT',
+          headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+          },
+        body: JSON.stringify({
+          name: res[0].name,
+          time: res[0].time,
+          location: res[0].location,
+          userEmail: res[0].userEmail,
+          date: res[0].date,
+          eventMembers: [result],
+        })
+        }).then(res => res.json()).then(console.log)) 
+      })
+    }
+
 
     render() {
 
-        const {params} = this.props.navigation.state;
+      const {params} = this.props.navigation.state;
+        
         return (
           <View style={{    
             marginTop: "3%",
