3fb9ccadef90fb5ac75bb3263677bcd078ef1e01
Christian Whitted <TheVoidLives@users.noreply.github.com>
2018-11-11 20:01:51 -0500

    add api event screens

--- QUp/screens/JoinEventScreen.js
+++ QUp/screens/JoinEventScreen.js
@@ -8,133 +8,134 @@ import {Marker} from 'react-native-maps';
 import {styles } from '../styles/eventlistscreen';
 import {saveUserId, getUserId, getUserLatLon} from '../utils/Storage';
 
- export default class JoinEventScreen extends React.Component {
+export default class JoinEventScreen extends React.Component {
 
-      constructor(props) {
+    constructor(props) {
         super(props);
-      this.state= {
-        eventMembers: [],
-        eventCoord: {
-        latitude: 0,
-        longitude: 0,
-        },
-      }
-      getUserLatLon().then((result) => {
+        this.state= {
+            eventMembers: [],
+            eventCoord: {
+                latitude: 0,
+                longitude: 0,
+            },
+        }
+        getUserLatLon().then((result) => {
             console.log(result.latitude);
             this.setState({eventCoord: {latitude: result.latitude, longitude: result.longitude}});
-         });
+        });
 
-      console.log(this.state);
-  }
+        console.log(this.state);
+    }
 
-     static navigationOptions = ({navigation}) => {
+    static navigationOptions = ({navigation}) => {
         return {
-          headerTitle: "Event Details",
-          headerRight: (
-            <View style={styles.buttonStyle}>
-              <Button onPress={() => {navigation.state.params.join()}}
-              title="Join Event!"
-              color="#53575e"
-              />
-            </View>
-          ),
+            headerTitle: "Event Details",
+            headerRight: (
+                <View style={styles.buttonStyle}>
+                <Button onPress={() => {navigation.state.params.join()}}
+                title="Join Event!"
+                color="#53575e"
+                />
+                </View>
+            ),
         };
-      };
+    };
 
-      componentDidMount() {
-       this.props.navigation.setParams({join: this.joinEvent.bind(this)})
+    componentDidMount() {
+        this.props.navigation.setParams({join: this.joinEvent.bind(this)})
     }
 
-     joinEvent() {
-      const {params} = this.props.navigation.state;
-      var apiKey = "6q0GvT04E_mFKH1XqLKO31Sw_6bw0i_Y";
-      var myDB = "qupdb";
-      var myCollection = "Events";
-      var eventId = params.item._id.$oid;
-      var url = "https://api.mlab.com/api/1/databases/"+myDB+"/collections/"+myCollection+"?q={_id:{$oid:'"+eventId+"'}}&apiKey="+apiKey
-      console.log(url);
-       fetch(url)
-      .then((res) => res.json())
-      .then((res) => {
-        console.log(res);
-        let arr = res[0].eventMembers;
-        console.log(arr);
+    joinEvent() {
+        const {params} = this.props.navigation.state;
+        //var apiKey = "6q0GvT04E_mFKH1XqLKO31Sw_6bw0i_Y";
+        //var myDB = "qupdb";
+        //var myCollection = "Events";
+        var eventId = params.item._id.$oid;
+        //var url = "https://api.mlab.com/api/1/databases/"+myDB+"/collections/"+myCollection+"?q={_id:{$oid:'"+eventId+"'}}&apiKey="+apiKey
+        var url = "https://104.248.112.100/events?id=" + eventId;
+        console.log(url);
+        fetch(url)
+            .then((res) => res.json())
+            .then((res) => {
+                console.log(res);
+                let arr = res[0].eventMembers;
+                console.log(arr);
 
-        getUserId().then((result) => {
-          arr.push(result);
-          userSet = new Set(arr);
-          console.log(Array.from(userSet));
-          let goodStuff = Array.from(userSet);
-          fetch(url ,{
-            method: 'PUT',
-            headers: {
-              Accept: 'application/json',
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-              name: res[0].name,
-              time: res[0].time,
-              eventCoord: res[0].eventCoord,
-              userEmail: res[0].userEmail,
-              eventString: res[0].eventString,
-              date: res[0].date,
-              eventMembers: goodStuff
-          })           
-        })
-      })
+                getUserId().then((result) => {
+                    arr.push(result);
+                    userSet = new Set(arr);
+                    console.log(Array.from(userSet));
+                    let goodStuff = Array.from(userSet);
+                    fetch(url ,{
+                        method: 'PUT',
+                        headers: {
+                            Accept: 'application/json',
+                            'Content-Type': 'application/json',
+                        },
+                        body: JSON.stringify({
+                            name: res[0].name,
+                            time: res[0].time,
+                            loc: { coordinates[res[0].eventCoord.longitude, res[0].eventCoord.latitude] },
+                            email: res[0].userEmail,
+                            eventString: res[0].eventString,
+                            date: res[0].date,
+                            members: goodStuff
+                        })           
+                    })
+                })
 
-        this.props.navigation.navigate('FullEventList');
-    })
+                this.props.navigation.navigate('FullEventList');
+            })
 
 
     }
-     
-     render() {
-         const {params} = this.props.navigation.state;
-         
+
+    render() {
+        const {params} = this.props.navigation.state;
+
 
         return (
-          
-          <View style={{    
-            marginTop: "3%",
-            alignContent: 'center',
-            flex: 1, height: 1,
-            width: "90%",
-            marginLeft: "5%",
-            marginRight: "5%",
-            fontSize: 20,
-          }}>
-             <Text style={{fontWeight: 'bold', fontSize: 40}}> 
-              {params.item.name} 
+
+            <View style={{    
+                marginTop: "3%",
+                    alignContent: 'center',
+                    flex: 1, height: 1,
+                    width: "90%",
+                    marginLeft: "5%",
+                    marginRight: "5%",
+                    fontSize: 20,
+            }}>
+            <Text style={{fontWeight: 'bold', fontSize: 40}}> 
+            {params.item.name} 
             </Text>
             <Text>Location: {params.item.eventString}</Text>
             <Text>Date: {params.item.date}</Text>
             <Text>Time: {params.item.time}</Text>
             <MapView style={{
-                    flex: 1,
+                flex: 1,
                     marginTop: "5%",
                     zIndex: -1
-                  }}
-                  initialRegion={{
-                    latitude: params.item.eventCoord.latitude,
+            }}
+            initialRegion={{
+                latitude: params.item.eventCoord.latitude,
                     longitude: params.item.eventCoord.longitude,
                     latitudeDelta: 0.1088,
                     longitudeDelta: 0.1821,
-                  }}
-                  
-                ><Marker
-      coordinate={params.item.eventCoord}
-      title="fancy Marker"
-      description="fuckadescr"
-    />
-    <Marker
-      coordinate={this.state.eventCoord}
-      title="My Location"
-      description="Im chillin here"
-      pinColor={"#339FFF"}
-    />
-        </MapView>
-           </View>
+            }}
+
+            ><Marker
+            coordinate={params.item.eventCoord}
+            title="fancy Marker"
+            description="fuckadescr"
+            />
+            <Marker
+            coordinate={this.state.eventCoord}
+            title="My Location"
+            description="Im chillin here"
+            pinColor={"#339FFF"}
+            />
+            </MapView>
+            </View>
         );
-      }
+    }
 } 
--- QUp/screens/NewEventScreen.js
+++ QUp/screens/NewEventScreen.js
@@ -12,235 +12,237 @@ import {saveUserId, getUserId} from '../utils/Storage';
 
 
 export default class NewEventScreen extends React.Component {
-  constructor(props){
-    super(props);
-    this.state={
-      eventName:'',
-      eventTime: 'Choose Time',
-      eventDate: 'Choose Date',
-      userLat: '',
-      userLon: '',
-      eventString: 'Choose Location', 
-      eventCoord: {
-        latitude: 0,
-        longitude: 0,
-      },
-      userEmail: '',
-      cycler: false,
-      isDatePickerVisible: false,
-      isTimePickerVisible: false,
-      modalVisible: false,
+    constructor(props){
+        super(props);
+        this.state={
+            eventName:'',
+            eventTime: 'Choose Time',
+            eventDate: 'Choose Date',
+            userLat: '',
+            userLon: '',
+            eventString: 'Choose Location', 
+            eventCoord: {
+                latitude: 0,
+                longitude: 0,
+            },
+            userEmail: '',
+            cycler: false,
+            isDatePickerVisible: false,
+            isTimePickerVisible: false,
+            modalVisible: false,
+
+        }
+        Geocoder.init('AIzaSyCRPspfu6DgUDUBOeqTFZMWG_CbnKHUzd8');
+        navigator.geolocation.getCurrentPosition(
+            (position) => {
+                this.setState({userLat: position.coords.latitude, userLon: position.coords.longitude});
+
+
+                console.log(position.coords.latitude + "here is lat!");
+            },
+            (error) => {console.log(error)}
+        );
 
     }
-    Geocoder.init('AIzaSyCRPspfu6DgUDUBOeqTFZMWG_CbnKHUzd8');
-    navigator.geolocation.getCurrentPosition(
-      (position) => {
-        this.setState({userLat: position.coords.latitude, userLon: position.coords.longitude});
 
 
-        console.log(position.coords.latitude + "here is lat!");
-      },
-      (error) => {console.log(error)}
-    );
-
-  }
-  
-
-  static navigationOptions = ({navigation}) => {
+    static navigationOptions = ({navigation}) => {
         return {
-          headerTitle: "New Event",
-          headerRight: (
-            <View style={styles.buttonStyle}>
-              <Button onPress={() => {
-                if (navigation.state.params.create())
-                  
-                  navigation.navigate('Home');
-              }}
-              title="Create"
-              color="#53575e"
-              />
-             </View>
-           ),
+            headerTitle: "New Event",
+            headerRight: (
+                <View style={styles.buttonStyle}>
+                <Button onPress={() => {
+                    if (navigation.state.params.create())
+
+                        navigation.navigate('Home');
+                }}
+                title="Create"
+                color="#53575e"
+                />
+                </View>
+            ),
         };
-      };
+    };
 
-      componentDidMount() {
-       this.props.navigation.setParams({create: this.createEvent.bind(this)})
+    componentDidMount() {
+        this.props.navigation.setParams({create: this.createEvent.bind(this)})
     }
 
-  setModalVisible(visible) {
-    this.setState({modalVisible: visible});
-  }
-
-  _showSpinner = () => {this.setState({cycler: true}); console.log("called this function" + JSON.stringify(this.state));};
-
-  _showDatePicker = () => this.setState({ isDatePickerVisible: true });
-
-  _hideDatePicker = () => this.setState({ isDatePickerVisible: false });
-
-  _showTimePicker = () => this.setState({ isTimePickerVisible: true });
-
-  _hideTimePicker = () => this.setState({ isTimePickerVisible: false });
-
-  _handleDatePicked = (date) => {
-    console.log('A date has been picked: ' + date);
-    let constructedDate = `${date.getMonth()+1}/${date.getDate()}/${date.getFullYear()}`
-    this.setState({eventDate: constructedDate});
-    this._hideDatePicker();
-  };
-
-  _handleTimePicked = (time) => {
-    console.log('A time has been picked'+ time);
-    let constructedTime = `${time.getHours()}:${time.getMinutes()}`
-    this.setState({eventTime: constructedTime});
-    this._hideTimePicker();
-    // style={{fontFamily: 'sans-serif-light', marginTop: 15, marginRight: 30, borderBottomColor: '#575f6b', borderBottomWidth: 1}}
-  };
-
-  createEvent(){
-
-    if (this.state.eventName === '' || this.state.eventString === '' || this.state.eventDate === 'Choose Date' || this.state.eventTime === 'Choose Time'){
-      alert('Please fill out all fields before continuing');
-      return false;
+    setModalVisible(visible) {
+        this.setState({modalVisible: visible});
     }
 
+    _showSpinner = () => {this.setState({cycler: true}); console.log("called this function" + JSON.stringify(this.state));};
 
-    var apiKey = "6q0GvT04E_mFKH1XqLKO31Sw_6bw0i_Y";
-      var myDB = "qupdb";
-      var myCollection = "Events";
-      console.log(this.state);
-      getUserId().then((result) => {
-      fetch("https://api.mlab.com/api/1/databases/"+myDB+"/collections/"+myCollection+"?apiKey="+apiKey,{
-          method: 'POST',
-          headers: {
-            Accept: 'application/json',
-            'Content-Type': 'application/json',
-          },
-        body: JSON.stringify({
-        name: this.state.eventName,
-        time: this.state.eventTime,
-        eventCoord: this.state.eventCoord,
-        userEmail: result,
-        eventString: this.state.eventString,
-        date: this.state.eventDate,
-        eventMembers: [result]
-      })
+    _showDatePicker = () => this.setState({ isDatePickerVisible: true });
 
-    })}).catch(error => console.log(error));
+    _hideDatePicker = () => this.setState({ isDatePickerVisible: false });
 
-      return true;
-  }
+    _showTimePicker = () => this.setState({ isTimePickerVisible: true });
 
-  render(){
-     return(
-    
-          <KeyboardAvoidingView style={{justifyContent: 'center', flex: 1, height: 1,
-          width: "90%",
-          marginLeft: "5%",}} enabled>
+    _hideTimePicker = () => this.setState({ isTimePickerVisible: false });
+
+    _handleDatePicked = (date) => {
+        console.log('A date has been picked: ' + date);
+        let constructedDate = `${date.getMonth()+1}/${date.getDate()}/${date.getFullYear()}`
+        this.setState({eventDate: constructedDate});
+        this._hideDatePicker();
+    };
+
+    _handleTimePicked = (time) => {
+        console.log('A time has been picked'+ time);
+        let constructedTime = `${time.getHours()}:${time.getMinutes()}`
+        this.setState({eventTime: constructedTime});
+        this._hideTimePicker();
+        // style={{fontFamily: 'sans-serif-light', marginTop: 15, marginRight: 30, borderBottomColor: '#575f6b', borderBottomWidth: 1}}
+    };
+
+    createEvent(){
+
+        if (this.state.eventName === '' || this.state.eventString === '' || this.state.eventDate === 'Choose Date' || this.state.eventTime === 'Choose Time'){
+            alert('Please fill out all fields before continuing');
+            return false;
+        }
+
+
+        //var apiKey = "6q0GvT04E_mFKH1XqLKO31Sw_6bw0i_Y";
+        //var myDB = "qupdb";
+        //var myCollection = "Events";
+        console.log(this.state);
+        getUserId().then((result) => {
+            fetch("https://104.248.112.100/events", {
+                method: 'POST',
+                headers: {
+                    Accept: 'application/json',
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    name: this.state.eventName,
+                    time: this.state.eventTime,
+                    loc: { 
+                        coordinates[this.state.eventCoord.longitude, this.state.eventCoord.latitude] 
+                    }
+                    email: result,
+                    eventString: this.state.eventString,
+                    date: this.state.eventDate,
+                    eventMembers: [result]
+                })
+
+            })}).catch(error => console.log(error));
+
+        return true;
+    }
+
+    render(){
+        return(
+
+            <KeyboardAvoidingView style={{justifyContent: 'center', flex: 1, height: 1,
+                    width: "90%",
+                    marginLeft: "5%",}} enabled>
             <FormLabel>Name</FormLabel>
-            
+
             <FormInput placeholder="My Awesome Event" 
             onChangeText={(value) => this.setState({eventName: value})}/>
-      
-      
-              <FormLabel>Location</FormLabel>
-              <TouchableHighlight
-                onPress={() => {
-                  this.setModalVisible(!this.state.modalVisible);
-                }}>
-                <Text style={styles.eventAddressStyle}>{this.state.eventString}</Text>
-              </TouchableHighlight>
-              
-              <Modal
-          transparent={false}
-          visible={this.state.modalVisible}
-          onRequestClose={() => {
-            Alert.alert('Modal has been closed.');
-          }}>
-          <View style={{
-          flex: 1,
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center'}}>
-          <MapView style= {styles.map}
-                  initialRegion={{
-                    latitude: this.state.userLat,
+
+
+            <FormLabel>Location</FormLabel>
+            <TouchableHighlight
+            onPress={() => {
+                this.setModalVisible(!this.state.modalVisible);
+            }}>
+            <Text style={styles.eventAddressStyle}>{this.state.eventString}</Text>
+            </TouchableHighlight>
+
+            <Modal
+            transparent={false}
+            visible={this.state.modalVisible}
+            onRequestClose={() => {
+                Alert.alert('Modal has been closed.');
+            }}>
+            <View style={{
+                flex: 1,
+                    flexDirection: 'column',
+                    justifyContent: 'center',
+                    alignItems: 'center'}}>
+            <MapView style= {styles.map}
+            initialRegion={{
+                latitude: this.state.userLat,
                     longitude: this.state.userLon,
                     latitudeDelta: 0.0922,
                     longitudeDelta: 0.0421,
-                  }}
-                  onPress={ (event) => {
-                    this.setState({ eventCoord: event.nativeEvent.coordinate})
-                  } }
-                ><Marker
-      coordinate={this.state.eventCoord}
-      title="fancy Marker"
-      description="fuckadescr"
-    />
-        </MapView>
+            }}
+            onPress={ (event) => {
+                this.setState({ eventCoord: event.nativeEvent.coordinate})
+            } }
+            ><Marker
+            coordinate={this.state.eventCoord}
+            title="fancy Marker"
+            description="fuckadescr"
+            />
+            </MapView>
             <View style={{flex: 1}}>
-              
+
             <View style={styles.buttonStyle}>
 
-              <Button  onPress={() => {
-                  this._showSpinner();
-                  console.log(this.state);
-                  Geocoder.from(this.state.eventCoord.latitude, this.state.eventCoord.longitude)
-        .then(json => {
-          var addressComponent = json.results[0].address_components;
-            this.setState({eventString: addressComponent[1].short_name + ' \n' + addressComponent[2].short_name + ', '+ addressComponent[4].short_name})
-            console.log(addressComponent);
-            this.setState({cycler: !this.state.cycler});
-            this.setModalVisible(!this.state.modalVisible);
-            
-        })
-        .catch(error => console.warn(error));
-                }} title="Set Location">
-               
-              </Button>
-              <ActivityIndicator size="large" color="#0020ff" animating={this.state.cycler}/>
-              
-              </View>
-              <View style={styles.loadingStyle}>
-              
-              </View>
+            <Button  onPress={() => {
+                this._showSpinner();
+                console.log(this.state);
+                Geocoder.from(this.state.eventCoord.latitude, this.state.eventCoord.longitude)
+                    .then(json => {
+                        var addressComponent = json.results[0].address_components;
+                        this.setState({eventString: addressComponent[1].short_name + ' \n' + addressComponent[2].short_name + ', '+ addressComponent[4].short_name})
+                        console.log(addressComponent);
+                        this.setState({cycler: !this.state.cycler});
+                        this.setModalVisible(!this.state.modalVisible);
+
+                    })
+                    .catch(error => console.warn(error));
+            }} title="Set Location">
+
+            </Button>
+            <ActivityIndicator size="large" color="#0020ff" animating={this.state.cycler}/>
+
             </View>
-          </View>
-        </Modal>
-            
+            <View style={styles.loadingStyle}>
+
+            </View>
+            </View>
+            </View>
+            </Modal>
+
             <View style={{flex: 1, flexDirection: 'row'}} enabled>
             <View style={{flex: 1}}>
             <FormLabel>Date</FormLabel>
             <TouchableOpacity onPress={this._showDatePicker}>
             <Text style={styles.dateTimeStyle}>{this.state.eventDate}</Text>
-        </TouchableOpacity>
-        <DateTimePicker
-          isVisible={this.state.isDatePickerVisible}
-          onConfirm={this._handleDatePicked}
-          onCancel={this._hideDatePicker}
-         
-        />
-        </View>
+            </TouchableOpacity>
+            <DateTimePicker
+            isVisible={this.state.isDatePickerVisible}
+            onConfirm={this._handleDatePicked}
+            onCancel={this._hideDatePicker}
+
+            />
+            </View>
             <View style={{flex: 1}}>
             <FormLabel>Time</FormLabel>
-        <TouchableOpacity onPress={this._showTimePicker}>
+            <TouchableOpacity onPress={this._showTimePicker}>
             <Text style={styles.dateTimeStyle}>{this.state.eventTime}</Text>
-        </TouchableOpacity>
-        <DateTimePicker
-          isVisible={this.state.isTimePickerVisible}
-          onConfirm={this._handleTimePicked}
-          onCancel={this._hideTimePicker}
-          mode="time"
-        />
-        </View>
-        </View>
-          
-          
-          
+            </TouchableOpacity>
+            <DateTimePicker
+            isVisible={this.state.isTimePickerVisible}
+            onConfirm={this._handleTimePicked}
+            onCancel={this._hideTimePicker}
+            mode="time"
+            />
+            </View>
+            </View>
 
-    
-          </KeyboardAvoidingView>
-      
-    )
-  }
+
+
+
+
+            </KeyboardAvoidingView>
+
+        )
+    }
 }
